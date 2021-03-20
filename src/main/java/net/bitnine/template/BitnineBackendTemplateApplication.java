777c4ca0234ed8f1a551d58fc7b8f079ef8663c7
Changhyun.Kim <karria0525@gmail.com>
2021-03-20 20:00:04 +0900

    Initial commit

---
+++ src/main/java/net/bitnine/template/BitnineBackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package net.bitnine.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitnineBackendTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitnineBackendTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bitnine/template/BitnineBackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bitnine.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitnineBackendTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
